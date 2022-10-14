025b711bb35dbb840ec34bd82f0580b08fbc0517
s-mdhaffar <s.mdhaffar@gmail.com>
2022-10-14 13:00:18 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {ref,onMounted,computed,watch} from 'vue'
+
+const todos=ref([])
+const name=ref('')
+
+const input_content=ref('')
+const input_category=ref(null)
+
+const todo_asc=computed(()=> todos.value.sort((a,b)=>{
+  return a.createdAt - b.createdAt
+}))
+
+watch(name,(newVal)=>{
+  localStorage.setItem('name',newVal)
+})
+
+onMounted(()=>{
+  name.value=localStorage.getItem('name') || ''
+})
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up <input type="text" placeholder="Name here" v-model="name">
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h4>What's on your todo list?</h4>
+      <input 
+      type="text"
+      placeholder="e.g. make a video"
+      v-model="input_content">
+      
+    </section>
+  </main>
+</template>
